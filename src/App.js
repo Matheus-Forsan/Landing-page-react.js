129212a4787c878d66c11a02ebfd418784aa3dbb
matheus marques <matheusforsan@gmail.com>
2024-08-01 08:39:30 -0300

    Profissão

--- src/App.js
+++ src/App.js
@@ -5,19 +5,42 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Desenvolvedor de Software</h1>
+        <p>O desenvolvimento de software é o processo técnico (e também criativo) de transformar ideias tanto em aplicações quanto programas que facilitam as mais variadas atividades do dia-a-dia. 
+        Dessa forma, os profissionais dessa área são responsáveis por escrever o código, testar e otimizar programas diversos, desde aplicativos móveis até sistemas complexos de gerenciamento empresarial. </p>
       </header>
+      <div className='App-body'>
+        <h2>Habilidades necessarias:</h2>
+        <ul>
+          <li>Habilidade de resolução de problemas</li>
+          <li>Conhecimento técnico em linguagens de programação e frameworks</li>
+          <li>Familiaridade com bancos de dados</li>
+          <li>Conhecimento de metodologias ágeis </li>
+          <li>Lógica de programação e algoritmos</li>
+          <li>Compreensão de estruturas de dados e bancos de dados</li>
+          <li>Habilidades de comunicação e trabalho em equipe</li>
+          <li>Conhecimento em testes de software</li>
+          <li>Entendimento de princípios de design e UX/UI</li>
+          <li>Habilidades de gerenciamento de tempo e pensamento crítico</li>
+          <li>Aprendizado contínuo e lidar com pressão.</li>
+        </ul>
+        <h2>Oportidades:</h2>
+        <p>Conseguindo cumprir a função de um Desenvolvedor de software você tambem sera capaz de exercer outras profissões como Engenheiro de software, Gerente de TI, Desenvolvedor python, java, .NET, RPA e especialista DevOps entre outras.</p> 
+        <h2>Ferramentas:</h2>
+        <p>Tendo em vista que sua profissão usara um computador as ferramentas serão aplicações como:</p>
+        <ul>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Jira</li>
+        <li>Slack</li>
+        <li>Jenkins</li>
+        <li>Docker</li>
+        <li>Postman</li>
+        <li>Selenium</li>
+        <li>Apache Kafka</li>
+        <li>GitLab</li>
+      </ul>
+      </div>
     </div>
   );
 }
